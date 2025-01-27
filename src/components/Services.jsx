c7903332e22b66d6d301bc46a847eac76bf01a3c
gaintopskills <raynafit@yahoo.com>
2025-01-27 12:47:23 -0800

    1

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,42 +7,42 @@ const services = [
     description:
       "We offer high-quality embroidery from custom patches to intricate applications for polo shirts, jackets, caps, workwear, and business shirts.",
     image: "/icons/embroidery-services.webp",
-    link: "https://theprinthq.com.au/embroidery-services/",
+    link: "/custom-embroidery-services-high-volume/",
   },
   {
     title: "Screen Printing",
     description:
       "We deliver best and competative screen printing products. We are always in the process of collaborating between our sales team and clients to acieve the best color matching and quality ensuring excellent results every time.",
     image: "/icons/bulk-screen-printing-services.webp",
-    link: "https://theprinthq.com.au/screen-printing-sydney/",
+    link: "/screen-printing-dirrect-to-guarment/",
   },
   {
     title: "DTG - Direct To Garment",
     description:
       "We specialize in delivering high-quality DTG (Direct To Garment) printing, offering vibrant and durable designs with a smooth finish. Perfect for promotional products, fashion events, or custom clothing lines, we ensure exceptional detail and color accuracy on every garment.",
     image: "/icons/direct-to-garment-services.webp",
-    link: "https://theprinthq.com.au/dtg-printing-sydney/",
+    link: "/los-angeles-screen-printing-direct-to-garment-dtg/",
   },
   {
     title: "Digital Sublimation",
     description:
       "We utilize state-of-the-art Mimaki and Italian Monti Antonio machines to deliver top-quality digital sublimation services. All production is done in-house, ensuring exceptional results with speed and precision!",
       image: "/icons/digital-sublimation-services.webp",
-      link: "https://theprinthq.com.au/dtf-printing/",
+      link: "/los-angeles-digital-sublimation-printing/",
   },
   {
     title: "Cut and Sew",
     description:
       "Our Cut and Sew services provide complete customization, from pattern creation to finished products. Whether it's unique apparel, uniforms, or tailored designs, we ensure precision, quality, and attention to detail in every stitch.",
     image: "/icons/cut-and-sew.webp",
-    link: "https://theprinthq.com.au/vinyl-heat-transfers/",
+    link: "/cut-sew/",
   },
   {
     title: "Heat Transfer",
     description:
       "Premium full-color heat transfer services tailored for businesses, sports teams, fashion, and special events. Enjoy competitive pricing, quick turnaround times, and exceptional customer service.",
     image: "/icons/heat-tranfer.webp",
-    link: "https://theprinthq.com.au/blank-apparel/",
+    link: "los-angeles-heat-transfer/",
   },
 ];
 export const ServicesSection = () => {
