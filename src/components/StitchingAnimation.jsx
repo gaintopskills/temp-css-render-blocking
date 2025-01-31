f9cfd6ce0aee45f474ae14bb38edaaedc19c0960
gaintopskills <raynafit@yahoo.com>
2025-01-30 16:24:53 -0800

    1

--- src/components/StitchingAnimation.jsx
+++ src/components/StitchingAnimation.jsx
@@ -24,6 +24,31 @@ export const StitchingAnimation = () => {
         
         }}
       />
+  {/* Scroll Down Indicator (Appears After Image Loads) */}
+  <div
+        className={`absolute bottom-10 flex justify-center transition-opacity duration-1000 ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <div className="animate-bounce">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-10 h-10 text-white opacity-40 "
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default StitchingAnimation;
